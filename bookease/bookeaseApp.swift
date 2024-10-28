1e1509319d29c0d34e71cf83eb5ea4d4e88c32c6
Farooq Ameen <a00173@aubh.edu.bh>
2024-10-28 14:45:13 +0300

    Initial Commit

---
+++ bookease/bookeaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookeaseApp.swift
+//  bookease
+//
+//  Created by Aubh.admin on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct bookeaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bookeaseTests/bookeaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  bookeaseTests.swift
+//  bookeaseTests
+//
+//  Created by Aubh.admin on 28/10/2024.
+//
+
+import Testing
+@testable import bookease
+
+struct bookeaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
